5bd3ee880076fb424e489cc98590ebeea02f7b89
sonuvarg <51948023+sonuvarg@users.noreply.github.com>
2019-06-27 16:06:39 +0530

    3 files added

---
+++ Testing_Demo.java
@@ -0,0 +1,1 @@
+Class Variabe_Basic
---
+++ Variable_Basic.java
@@ -0,0 +1,7 @@
+public class Variable_Basic {
+  public static void main(String[] args) {
+    int x;
+    x=10;
+    System.out.println("int var x=" +x);
+  }
+}
